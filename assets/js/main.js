2ddf297e635c14d23a3903ea455f33c759d1b9db
bruce.lam <bruce.lam@websparks.sg>
2023-07-15 16:06:59 +0700

    init template

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+
+jQuery(document).ready(function($){
+   $('.slider_carusal_gallery_bank_content').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+   $('.slider_product_home').slick({
+  infinite: true,
+  speed: 300,
+   });
+
+});
